61b2ccb6b8f479ce675cd918beead77c81db0909
JakeHU1 <43312270+JakeHU1@users.noreply.github.com>
2018-10-18 13:37:48 +0200

    Update PROG.py
    Nu met link naar de GUI

--- PROG.py
+++ PROG.py
@@ -1,8 +1,18 @@
 import random
+from tkinter import *
+
 def randomHero():
-    'Deze functie werkt met een lijst van 10 heroes afkomstig uit de API. Vervolgens wordt uit deze API 10 heroes gekozen en hiervoor wordt een willekeurige geselecteerd'
+    'Deze functie werkt met een lijst van 10 heroes afkomstig uit de API. Vervolgens wordt uit deze API 10 heroes gekozen en hiervoor wordt een willekeurige gelecteerd'
     lst_of_heroes = ['Hero 1', 'Hero 2', 'Hero 3', 'Hero 4', 'Hero 5', 'Hero 6', 'Hero 7', 'Hero 8', 'Hero 9', 'Hero 10'] #Deze data moet van de API komen!
-    number = random.randint(1,10)
-    print(lst_of_heroes[number])
-public_key = '56235b4ce6e75a0b4bb2d844d6c7abf8'
-private_key = '66072693baa29d8c45e1f117d84b2eeda0fa66a4' #Voor de API
+    number = random.randint(0,9)
+    label["text"] = lst_of_heroes[number]
+
+root = Tk()
+
+button = Button(master=root, text="Random Hero", command=randomHero)
+button.pack()
+
+label = Label(master=root)
+label.pack()
+
+root.mainloop()
