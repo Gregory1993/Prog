4ac0830f6270347bcefc97acc42490b0bbd9e98b
JesseWils <jessewils42@gmail.com>
2018-10-24 11:47:48 +0200

    api gedoe

--- APIcall.py
+++ APIcall.py
@@ -3,6 +3,7 @@ import hashlib
 import time
 import requests
 
+
 public_key = "e236fca8413247cbbd3b6eab5a278226"
 private_key = "52fd65bb2019b663e26f555526b943dcfc1d5b2d"
 timestamp = str(time.time())
@@ -13,26 +14,34 @@ url = "http://gateway.marvel.com:80/v1/public/characters"
 connection_url = url+"?ts="+timestamp+"&apikey="+public_key+"&hash="+md5digest
 print(connection_url)
 
+
 response = requests.get(connection_url)
 jsontext = json.loads(response.text)
 
 # om de JSON leesbaar te printen...
 print(json.dumps(jsontext, sort_keys=True, indent=4))
+json_leesbaar = json.dumps(jsontext, sort_keys=True, indent=4)
+
+with open('apitext.json', 'w') as myfile:
+    myfile.write(json_leesbaar)
+
 
 
 print("\nGevonden characters in comics:")
 # JSON-indeling kun je uit het geprinte resultaat halen of uit de Marvel docs!
-for item in jsontext['data']['results'][0]['comics']['items']:
+for item in jsontext['data']['results']:
     print(item['name'])
+
+
+
 
 print("\nGevonden characters in series:")
 # JSON-indeling kun je uit het geprinte resultaat halen of uit de Marvel docs!
-for item in jsontext['data']['results'][0]['series']['items']:
+for item in jsontext['data']['results']:
     print(item['name'])
 
-
-
-
-
-
-
+with open('api_data.json', 'a') as file:
+    for item in jsontext['data']['results']:
+        file.write(item['name'] + '\n')
+    for item in jsontext['data']['results']:
+        file.write(item['id'])
