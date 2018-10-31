3138443281c3686b5847c92c9acdb949b1a8ee6b
esref7 <43609388+esref7@users.noreply.github.com>
2018-10-31 23:16:51 +0100

    Add files via upload

---
+++ PROG MINIPROJECT FINAL.py
@@ -0,0 +1,83 @@
+import csv
+from threading import Timer
+
+while True:
+
+    fieldnames = ['type', 'username', 'score', 'ID']
+    username = input("What is your name? ")
+    score = int(input("What is your score? "))
+
+    class Structure:
+        username = ""
+        score = int(0)
+        ID = int(0)
+
+
+    alltime = Structure()
+    daily = Structure()
+
+
+
+    def getOldScores():
+        with open('punten.csv') as file:
+            csv_reader = csv.DictReader(file, delimiter=',')
+            for row in csv_reader:
+                if row["type"] == "alltime":
+                    alltime.username = row["username"]
+                    alltime.score = int(row["score"])
+                elif row["type"] == "daily":
+                    daily.username = row["username"]
+                    daily.score = int(row["score"])
+                    daily.ID = int(row["ID"])
+
+
+    # noinspection PyTypeChecker
+    def checkScores():
+        getOldScores()
+        if score > alltime.score:
+            sendMessage(0)
+            alltime.username = username
+            alltime.score = score
+            daily.username = username
+            daily.score = score
+            daily.ID = daily.ID + 1
+            writeChanges()
+            t = Timer(5, resetDaily, args=str(daily.ID))
+            t.start()
+        elif score > daily.score:
+            sendMessage(1)
+            daily.username = username
+            daily.score = score
+            daily.ID = daily.ID + 1
+            writeChanges()
+            t = Timer(5, resetDaily, args=str(daily.ID))
+            t.start()
+        else:
+            sendMessage(2)
+
+
+    def writeChanges():
+        with open (punten.csv', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'type': 'alltime', 'username': alltime.username, 'score': alltime.score})
+            writer.writerow({'type': 'daily', 'username': daily.username, 'score': daily.score, 'ID': daily.ID})
+
+
+    def sendMessage(result):
+        if result == 0:
+            print("New alltime highscore achieved! ")
+        elif result == 1:
+            print("New daily highscore achieved!")
+        elif result == 2:
+            print("Better luck next time!")
+
+    def resetDaily(dailyID):
+        getOldScores()
+        if int(dailyID) == int(daily.ID):
+            daily.username = "none"
+            daily.score = int(0)
+            writeChanges()
+
+
+    checkScores()
